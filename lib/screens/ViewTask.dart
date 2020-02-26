b8cfd195aa4b0c6d8d9167c8e5f75dfe05bf8287
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2020-02-26 16:05:50 +0400

    Fix profile

--- lib/screens/ViewTask.dart
+++ lib/screens/ViewTask.dart
@@ -24,84 +24,84 @@ class _ViewTaskScreenState extends State<ViewTaskScreen> {
       appBar: MyAppBar(
         title: "Task details",
       ),
-      body: Container(
-        margin: EdgeInsets.symmetric(vertical: 35.0, horizontal: sWidth / 9),
-        child: Column(
-          children: <Widget>[
-            Expanded(
-              flex: 2,
-              child: GFImageOverlay(
-                  height: sHeight * 2 / 4,
-                  width: sWidth,
-                  image: NetworkImage(task["imageUrl"]),
-                  borderRadius: BorderRadius.circular(20.0)),
-            ),
-            Expanded(
-              flex: 2,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  GFTypography(
-                    text: task["title"],
-                    type: GFTypographyType.typo1,
-                    showDivider: false,
-                  ),
-                  Row(
-                    children: <Widget>[
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.location_on,
-                            color: kMainColor,
-                            size: iSize,
-                          ),
-                          Text(" " + task['location']),
-                        ],
-                      ),
-                      SizedBox(
-                        width: 25.0,
-                      ),
-                      Row(
-                        children: <Widget>[
-                          Icon(
-                            Icons.calendar_today,
-                            color: kMainColor,
-                            size: iSize,
-                          ),
-                          Text(" " + task['date']),
-                        ],
-                      ),
-                    ],
-                  ),
-                  GFTypography(
-                    text: task["description"],
-                    type: GFTypographyType.typo4,
-                    showDivider: false,
-                  ),
-                  Row(
-                    children: generateTagsFromStrings(task['tags']),
-                  ),
-                  ButtonTheme(
-                    padding: EdgeInsets.all(15.0),
-                    minWidth: double.infinity,
-                    buttonColor: kMainColor,
-                    child: RaisedButton(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: new BorderRadius.circular(15.0),
-                      ),
-                      onPressed: () {},
-                      child:
-                          Text("Apply", style: TextStyle(color: Colors.white)),
-                    ),
-                  )
-                ],
+      body: SingleChildScrollView(
+        child: Container(
+          height: sHeight,
+          margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: sWidth / 14),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: sHeight * 1 / 3,
+                child: GFImageOverlay(
+                    image: NetworkImage(task["imageUrl"]),
+                    borderRadius: BorderRadius.circular(15.0)),
               ),
-            )
-          ],
+              Container(
+                height: sHeight * 1.5 / 3,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    GFTypography(
+                      text: task["title"],
+                      type: GFTypographyType.typo1,
+                      showDivider: false,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.location_on,
+                              color: kMainColor,
+                              size: iSize,
+                            ),
+                            Text(" " + task['location']),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.calendar_today,
+                              color: kMainColor,
+                              size: iSize,
+                            ),
+                            Text(" " + task['date']),
+                          ],
+                        ),
+                        Text("Rs " + task['price'].toString())
+                      ],
+                    ),
+                    GFTypography(
+                      text: task["description"],
+                      type: GFTypographyType.typo4,
+                      showDivider: false,
+                    ),
+                    Row(
+                      children: generateTagsFromStrings(task['tags']),
+                    ),
+                    ButtonTheme(
+                      padding: EdgeInsets.all(15.0),
+                      minWidth: double.infinity,
+                      buttonColor: kMainColor,
+                      child: RaisedButton(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: new BorderRadius.circular(15.0),
+                        ),
+                        onPressed: () {},
+                        child: Text("Apply",
+                            style: TextStyle(color: Colors.white)),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
