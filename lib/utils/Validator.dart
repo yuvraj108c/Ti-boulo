60b1f0c4e0fcb182de62f67a823c211493c258ec
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2020-03-01 11:02:36 +0400

    Register page

--- lib/utils/Validator.dart
+++ lib/utils/Validator.dart
@@ -2,4 +2,8 @@ class Validator {
   static String emptyValidator(String field, String value) {
     return (value == null || value == "") ? field + " cannot be blank." : null;
   }
+
+  static bool isLessThan(String value, int characterCount) {
+    return value.length < characterCount;
+  }
 }
