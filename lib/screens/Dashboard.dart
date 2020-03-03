c33220189e7a234b42d6c93864c3f362f037f663
yuvraj <yuvraj.seegolam@umail.uom.ac.mu>
2020-03-03 23:49:13 +0400

    display events

--- lib/screens/Dashboard.dart
+++ lib/screens/Dashboard.dart
@@ -23,8 +23,8 @@ class _DashboardScreenState extends State<DashboardScreen> {
 
 class MyDashboard extends StatelessWidget {
   final Map<DateTime, List> _events = {
-    DateTime(2020, 3, 1): ['New Year\'s Day'],
-    DateTime(2020, 3, 6): ['Epiphany'],
+    DateTime(2020, 3, 1): ['Construct wall'],
+    DateTime(2020, 3, 2): ['Paint wall'],
   };
 
   @override
--- lib/widgets/MyCalendar.dart
+++ lib/widgets/MyCalendar.dart
@@ -13,11 +13,15 @@ class MyCalendar extends StatefulWidget {
 
 class _MyCalendarState extends State<MyCalendar> {
   var _calendarController;
+  List _selectedEvents;
 
   @override
   void initState() {
     super.initState();
+
+    final _selectedDay = DateTime.now();
     _calendarController = CalendarController();
+    _selectedEvents = widget.events[_selectedDay] ?? [];
   }
 
   @override
@@ -26,16 +30,46 @@ class _MyCalendarState extends State<MyCalendar> {
     super.dispose();
   }
 
+  void _onDaySelected(DateTime day, List events) {
+    setState(() {
+      _selectedEvents = events;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return TableCalendar(
-      calendarController: _calendarController,
-      events: widget.events,
-      initialCalendarFormat: CalendarFormat.week,
-      calendarStyle: CalendarStyle(
-        todayColor: kMainColor,
-      ),
-      onDaySelected: (date, events) => {},
+    return Column(
+      children: <Widget>[
+        TableCalendar(
+          calendarController: _calendarController,
+          events: widget.events,
+          initialCalendarFormat: CalendarFormat.week,
+          calendarStyle: CalendarStyle(
+            todayColor: kMainColor,
+          ),
+          onDaySelected: (date, events) => _onDaySelected(date, events),
+        ),
+        Expanded(child: _buildEventList())
+      ],
+    );
+  }
+
+  Widget _buildEventList() {
+    return ListView(
+      children: _selectedEvents
+          .map((event) => Container(
+                decoration: BoxDecoration(
+                  border: Border.all(width: 0.8),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+                child: ListTile(
+                  title: Text(event.toString()),
+                  onTap: () => print('$event tapped!'),
+                ),
+              ))
+          .toList(),
     );
   }
 }
